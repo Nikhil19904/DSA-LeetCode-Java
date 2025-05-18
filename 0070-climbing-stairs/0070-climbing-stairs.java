2d14271d12e080b7ce18429b7c29813608f79f21
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2025-05-18 13:28:47 +0530

    Time: 0 ms (100%), Space: 40.5 MB (50.64%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int climbStairs(int n) {
+    //     Map<Integer, Integer> memo = new HashMap<>();
+    //     return climbStairs(n, memo);
+    // }
+
+        // // recursive approach
+        // if(n==0 || n==1){
+        //     return 1;
+        // }
+
+        // return climbStairs(n-1) + climbStairs(n-2);
+        
+        // // memoization approach
+        // private int climbStairs(int n,Map<Integer,Integer>memo){
+        //      if (n == 0 || n == 1) {
+        //     return 1;
+        // }
+        // if (!memo.containsKey(n)) {
+        //     memo.put(n, climbStairs(n-1, memo) + climbStairs(n-2, memo));
+        // }
+        // return memo.get(n);
+        // }
+
+        if(n==0||n==1){
+            return 1;
+        }
+
+        int []dp=new int[n+1];
+        dp[0]=dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+    
+}
