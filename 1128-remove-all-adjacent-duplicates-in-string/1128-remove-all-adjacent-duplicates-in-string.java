74073ae2b67428f9e85826bcdde3ff427b45afd3
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2025-06-03 13:09:35 +0530

    Time: 36 ms (44.4%), Space: 45.6 MB (40.35%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public static String remAllAdjDupStr(String str){
+        Stack<Character>stk=new Stack<>();
+        StringBuilder sb=new StringBuilder("");
+        stk.push('{');
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==stk.peek()){
+                stk.pop();
+
+            }
+            else{
+                stk.push(str.charAt(i));
+            }
+        }
+
+        while(!stk.isEmpty()){
+            sb.append(stk.pop());
+        }
+        return str = sb.reverse().toString().substring(1);
+    }
+
+    public String removeDuplicates(String s) {
+        return remAllAdjDupStr(s);
+    }
+}
